24d881bfa8a2ed115402f6f88b9d569d2fad1585
Taz Campbell <shelbyacampbell2724@gmail.com>
2023-12-02 16:47:44 -0500

    balancedAsAllThingsShouldBe

--- app.js
+++ app.js
@@ -42,7 +42,41 @@ $(document).ready(() => {
         $li5.addClass('cat')
 
     const $li6 = $('<li>').text('Bertie Botts Every Flavor Beans')
+        $ul.append($li1, $li2, $li3, $li4, $li5, $li6)
 
-        $container.append($li1, $li2, $li3, $li4, $li5, $li6)
+    const $table = $('<table id="classes"></table>')
+        $container.append($table)
+
+    const $tableHead = $('<h5>').text('Spring 2017');
+        $tableHead.insertBefore($table)
+
+    const tableElement = $table
+    const theadElement = $('<thead>')
+        theadElement.append('<tr><th>Day</th><th>Classes</th></tr>')
+        tableElement.append(theadElement)
+
+    const $tr1 = $('<tr>')
+        $tr1.append('<td>Monday</td>')
+        $tr1.append('<td>Herbology, Defense Against the Dark Arts, Quidditch Practice</td>')
+
+    const $tr2 = $('<tr>')
+        $tr2.append('<td>Tuesday</td>')
+        $tr2.append('<td>Charms, Transfiguration</td>')
+
+    const $tr3 = $('<tr>')
+        $tr3.append('<td>Wednesday</td>')
+        $tr3.append('<td>Defense Against the Dark Arts, History of Magic</td>')
+
+    const $tr4 = $('<tr>')
+        $tr4.append('<td>Thursday</td>')
+        $tr4.append('<td>Potions, Herbology, Quidditch Practice</td>')
+
+    const $tr5 = $('<tr>')
+        $tr5.append('<td>Friday</td>')
+        $tr5.append('<td>History of Magic, Charms, Transfiguration</td>')
+        $table.append($tr1, $tr2, $tr3, $tr4, $tr5)
+
+
+
 }
 )
